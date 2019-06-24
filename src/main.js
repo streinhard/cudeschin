92ae5eb1207590b13e74fc3772b0ac02248a3a84
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2019-06-24 09:23:57 +0200

    First card

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from "vue";
+import Vue from 'vue';
 import Framework7 from 'framework7/framework7.esm.bundle.js';
 import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js';
 import 'framework7/css/framework7.bundle.css';
-import "./registerServiceWorker";
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
 Framework7.use(Framework7Vue);
 
-import App from "./app.vue";
+import './css/fonts.css';
+
+import App from './app.vue';
 
 new Vue({
   render: h => h(App),
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,10 +2,27 @@
     <f7-page>
         <f7-navbar title="Cudeschin"></f7-navbar>
 
-        <f7-block-title large>Willkommen!</f7-block-title>
-        <f7-block>
-            <p>Donec et nulla auctor massa pharetra adipiscing ut sit amet sem. Suspendisse molestie velit vitae mattis tincidunt. Ut sit amet quam mollis, vulputate turpis vel, sagittis felis.</p>
-        </f7-block>
+        <f7-card expandable :style="{height: '100px'}">
+            <f7-card-content :padding="false">
+                <div class="bg-color-green" :style="{height: '100px'}">
+                    <f7-card-header text-color="white" class="display-block">Aktivität planen</f7-card-header>
+                    <f7-link
+                        card-close
+                        color="black"
+                        class="card-opened-fade-in"
+                        :style="{position: 'absolute', right: '15px', top: '15px'}"
+                        icon-f7="close_round_fill"
+                    ></f7-link>
+                </div>
+                <div class="card-content-padding">
+                    <p>Bereits bei der Quartalsplanung habt ihr die Aktivität grob geplant. Diese Unterlagen dienen euch nun als Grundlage.</p>
+
+                    <h3>Ziele festlegen:</h3>
+                    <p>Was sollen die Teilnehmenden lernen und erleben?</p>
+                    <p>Was wollen wir als Leitungsteam erreichen?</p>
+                </div>
+            </f7-card-content>
+        </f7-card>
     </f7-page>
 </template>
 
