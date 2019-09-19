b4ea162c2eef069a0949866bf91297c509818b68
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2019-09-19 23:58:44 +0200

    Super creepy scout guy

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,9 @@
 <template>
     <f7-page>
         <f7-navbar>
+            <f7-nav-left>
+                <img src="/images/Pfadi.svg" style="height:28px; margin: 0px 0 0 16px">
+            </f7-nav-left>
             <f7-nav-title>Cudeschin</f7-nav-title>
             <f7-nav-right>
                 <f7-button :icon-f7="viewIcon" style="color:white" @click="toggleDisplay" />
