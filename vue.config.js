9a3d5775a6f8b49fef58c7e83eb2fb4d9c667907
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2020-08-04 20:04:02 +0200

    Fix android status bar color

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,18 @@ var path = require('path');
 
 module.exports = {
   lintOnSave: false,
+
   configureWebpack: {
     resolve: {
       alias: {
         src: path.resolve(__dirname, 'src')
       }
     },
+  },
+
+  pwa: {
+    name: 'cudeschin',
+    themeColor: '#00A0AF',
+    msTileColor: '#00A0AF'
   }
 };
