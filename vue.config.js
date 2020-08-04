470eef1c283b0f9d74c697218903d0da634a53a2
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2020-08-04 14:39:29 +0200

    Remove unneeded raw and markdown loader

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,6 @@ var path = require('path');
 
 module.exports = {
   lintOnSave: false,
-  chainWebpack: config => {
-    config.module
-        .rule('md')
-        .test(/\.md$/)
-        .use('raw-loader')
-            .loader('raw-loader')
-            .end()
-
-  },
   configureWebpack: {
     resolve: {
       alias: {
